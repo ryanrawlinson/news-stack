cd4b283a5d8ae2fa31297ea93938ab019eb08cff
Ryan Rawlinson <ryan.rawlinson@solera.com>
2018-05-09 14:23:44 -0500

    Added categories fragment.

--- app/src/main/java/com/rrawlinson/newsstack/HeadlinesFragment.kt
+++ app/src/main/java/com/rrawlinson/newsstack/HeadlinesFragment.kt
@@ -6,30 +6,35 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_headlines.*
+import android.widget.Button
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation.createNavigateOnClickListener
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class HeadlinesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_headlines, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getLatestHeadlinesButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.headlineDetailFragment, null))
+        val options = NavOptions.Builder()
+                .setEnterAnim(R.anim.nav_default_enter_anim)
+                .setExitAnim(R.anim.nav_default_exit_anim)
+                .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+                .setPopEnterAnim(R.anim.nav_default_pop_exit_anim)
+                .build()
+
+        // Navigate with nav controller and options
+//        view.findViewById<Button>(R.id.getLatestHeadlinesButton).setOnClickListener {
+//            findNavController(view).navigate(R.id.headlineDetailFragment, null, options)
+//        }
+
+        // Navigate with an action
+        view.findViewById<Button>(R.id.getLatestHeadlinesButton)
+                .setOnClickListener(createNavigateOnClickListener(R.id.action_headlinesFragment_to_headlineDetailFragment, null))
     }
 }
--- app/src/main/java/com/rrawlinson/newsstack/NewsCategoriesFragment.kt
+++ app/src/main/java/com/rrawlinson/newsstack/NewsCategoriesFragment.kt
@@ -17,12 +17,13 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class HeadlineDetailFragment : Fragment() {
+class NewsCategoriesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headline_detail, container, false)
+        return inflater.inflate(R.layout.fragment_news_categories, container, false)
     }
+
 
 }

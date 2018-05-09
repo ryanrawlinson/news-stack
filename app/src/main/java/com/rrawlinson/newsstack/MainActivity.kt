5a71594d3ac00c8c88b5b9c7b04021195fde4c4f
Ryan Rawlinson <ryan.rawlinson@solera.com>
2018-05-09 14:37:01 -0500

    Added simple bottom navigation implementation.

--- app/src/main/java/com/rrawlinson/newsstack/MainActivity.kt
+++ app/src/main/java/com/rrawlinson/newsstack/MainActivity.kt
@@ -3,30 +3,25 @@ package com.rrawlinson.newsstack
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val host = supportFragmentManager.findFragmentById(R.id.host_nav_fragment) as NavHostFragment? ?: return
+
+        setupBottomNavigation(host.navController)
+    }
+
+    private fun setupBottomNavigation(navController: NavController) {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).let { bottomNavigationView ->
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        }
     }
 }
